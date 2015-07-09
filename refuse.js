7ec1be45414297d3a45a70b6281b7d140bcbaa62
Arnout Kazemier <info@3rd-Eden.com>
2015-07-09 10:04:04 +0200

    [fix] Corrected rest argument syntax

--- refuse.js
+++ refuse.js
@@ -7,7 +7,7 @@
  * @returns {Object} A fully merged object.
  * @api private
  */
-export function assign(..args) {
+export function assign(...args) {
   return args.filter(Boolean).reduce(function merger(result, obj) {
     Object.keys(obj).forEach((key) => {
       if (obj[key] !== undefined) {
